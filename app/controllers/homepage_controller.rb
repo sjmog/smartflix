43b21782583609f2f9d9745f5d978789f8b0dccc
Sam Morgan <s_morgan@me.com>
2022-04-19 10:59:47 +0100

    Revert "Render shows to the homepage"

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,7 +1,4 @@
-require './lib/shows'
-
 class HomepageController < ApplicationController
   def index
-    @shows = Shows.from('./data/netflix_titles.csv', limit: 10)
   end
 end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,17 +1,8 @@
 require 'rails_helper'
-require './lib/shows'
 
-feature "Visiting the homepage" do
-  scenario "See the homepage header" do
+feature "Homepage" do
+  scenario "Visiting the homepage" do
     visit "/"
     expect(page).to have_content "Smartflix"
-  end
-
-  scenario "See the show titles in the CSV" do
-    visit "/"
-
-    Shows.from('data/netflix_titles.csv', limit: 10).each do |show|
-      expect(page).to have_content show.title
-    end
   end
 end
