7f72b53581f4702fa64579d31431824e18536247
sjmog <s_morgan@me.com>
2022-04-19 10:57:19 +0100

    Render shows to the homepage
    * Construct a nice little object to keep the controller clean
    * Present shows to the view with a nice interface
    * Limit the number of shows being rendered for memory purposes
    * All nicely tested :)

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,7 @@
+require './lib/shows'
+
 class HomepageController < ApplicationController
   def index
+    @shows = Shows.from('./data/netflix_titles.csv', limit: 10)
   end
 end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
+require './lib/shows'
 
-feature "Homepage" do
-  scenario "Visiting the homepage" do
+feature "Visiting the homepage" do
+  scenario "See the homepage header" do
     visit "/"
     expect(page).to have_content "Smartflix"
+  end
+
+  scenario "See the show titles in the CSV" do
+    visit "/"
+
+    Shows.from('data/netflix_titles.csv', limit: 10).each do |show|
+      expect(page).to have_content show.title
+    end
   end
 end
